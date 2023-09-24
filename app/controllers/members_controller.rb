ae427e1940f2653455aa7aaadf2d7e9fd375ff2c
“sparshalc” <imailsparsha@gmail.com>
2023-09-25 02:03:35 +0545

    Handled update operation for signed in user only

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,16 @@
 class MembersController < ApplicationController
+    before_action :authenticate_user!, only: [:edit_description, :update_description]
     def show
         @user = User.find(params[:id])
     end
     def edit_description
-        @user = User.find(params[:id])
+    end
+    def update_description
+        @user = current_user
+        respond_to do |format|
+            if @user.update(about: params[:user][:about])
+                format.turbo_stream
+            end
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/member/:id', to: 'members#show', as: 'member'
   get 'edit_description/:id', to: 'members#edit_description', as: 'edit_member_description'
+  patch 'update_description', to: 'members#update_description', as: 'update_member_description'
 end
