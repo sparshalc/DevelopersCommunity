4d05a639eac2fdb5fb13621186feb4a7dcd7f107
“sparshalc” <imailsparsha@gmail.com>
2023-09-25 22:58:43 +0545

    Added turbo-stream to update user details

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-    before_action :authenticate_user!, only: [:edit_description, :update_description]
+    before_action :authenticate_user!, only: [:edit_description, :update_description, :edit_personal_details, :update_personal_details]
     def show
         @user = User.find(params[:id])
     end
@@ -12,5 +12,19 @@ class MembersController < ApplicationController
                 format.turbo_stream
             end
         end
+    end
+    def edit_personal_details; end
+    def update_personal_details
+            @user = current_user
+            respond_to do |format|
+            if @user.update(user_personal_info_params)
+                format.turbo_stream
+            end
+        end
+    end
+
+    private
+    def user_personal_info_params
+        params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :pincode, :profile_title)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get '/member/:id', to: 'members#show', as: 'member'
+
   get 'edit_description/:id', to: 'members#edit_description', as: 'edit_member_description'
   patch 'update_description', to: 'members#update_description', as: 'update_member_description'
+
+  get 'edit_personal_details/:id', to: 'members#edit_personal_details', as: 'edit_member_personal_details'
+  patch 'update_personal_details', to: 'members#update_personal_details', as: 'update_member_personal_details'
 end
