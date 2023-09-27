f1187f1e82dd5c7943a1b9e54fa9b0dab155ab21
“sparshalc” <imailsparsha@gmail.com>
2023-09-27 23:52:07 +0545

    Implemented my network page for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
     def get_connection_status(current_user, user)
-        current_user.connections.find_by(connected_user_id: user.id).status
+        return nil if current_user == user
+        current_user.connections.find_by(connected_user_id: user.id).status 
+    end
+    def get_the_reciever(user_id)
+        User.find(user_id)
+    end
+    def get_the_requester(user_id)
+        User.find(user_id)
     end
 end
