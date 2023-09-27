eab5c656eacf03351137e1e02f50714a740cb229
“sparshalc” <imailsparsha@gmail.com>
2023-09-27 23:56:59 +0545

    Added Custom association to eliminate N+1 Query

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,4 @@ module ApplicationHelper
         return nil if current_user == user
         current_user.connections.find_by(connected_user_id: user.id).status 
     end
-    def get_the_reciever(user_id)
-        User.find(user_id)
-    end
-    def get_the_requester(user_id)
-        User.find(user_id)
-    end
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,5 +1,7 @@
 class Connection < ApplicationRecord
   belongs_to :user
+  belongs_to :requested, foreign_key: :connected_user_id, class_name: 'User'
+  belongs_to :received, foreign_key: :user_id, class_name: 'User'
 
   validates :connected_user_id, presence: true
   validates :status, presence: true, inclusion: { in: %w(pending accepted rejected deleted) }
